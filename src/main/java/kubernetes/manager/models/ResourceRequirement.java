00e537cbe0771e11bd66806074c9bf6b3cfa3319
senthuran16 <senthuranrc16@gmail.com>
2019-04-17 02:37:40 +0530

    Add ResourceRequirement bean

---
+++ src/main/java/kubernetes/manager/models/ResourceRequirement.java
@@ -0,0 +1,18 @@
+package kubernetes.manager.models;
+
+import java.util.Map;
+
+/**
+ * Contains information about a resource requirement of a child Siddhi app
+ */
+public class ResourceRequirement {
+    Map<String, String> labels;
+
+    public ResourceRequirement(Map<String, String> labels) {
+        this.labels = labels;
+    }
+
+    public Map<String, String> getLabels() {
+        return labels;
+    }
+}
